1da099d5a22cafc0a446d2c7675c876b405a6530
Amandine Arias <amandine.arias@gmail.com>
2021-09-30 13:01:44 +0200

    feat (component) : Menu

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../Assets/logo.svg';
+import '../Styles/menu.css';
 
 export default function Menu() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <span className="navbar-brand">
+        <Link className="navbar-brand" to="/">
           <img src={logo} alt="logo Sport See" />
-        </span>
+        </Link>
         {/* <button
           className="navbar-toggler"
           type="button"
@@ -21,19 +23,23 @@ export default function Menu() {
         </button> */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav w-100 d-flex justify-content-evenly">
-            <li className="nav-item">
-              <span className="nav-link active" aria-current="page">
-                Accueil
-              </span>
+            <NavLink className="nav-link" activeClassName="active" to="/">
+              Accueil
+            </NavLink>
+            <li className="nav-link">
+              {/* <NavLink className="nav-link"  activeClassName="active" to="/profil"> */}
+              Profil
+              {/* </NavLink> */}
             </li>
-            <li className="nav-item">
-              <span className="nav-link">Profil</span>
+            <li className="nav-link">
+              {/* <NavLink className="nav-link"  activeClassName="active" to="/reglages"> */}
+              Réglages
+              {/* </NavLink> */}
             </li>
-            <li className="nav-item">
-              <span className="nav-link">Réglages</span>
-            </li>
-            <li className="nav-item">
-              <span className="nav-link">Communauté</span>
+            <li className="nav-link">
+              {/* <NavLink className="nav-link"  activeClassName="active" to="/communaute"> */}
+              Communauté
+              {/* </NavLink> */}
             </li>
           </ul>
         </div>
