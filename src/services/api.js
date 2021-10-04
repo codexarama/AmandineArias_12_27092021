a4bd82692af5653c0de55fe4fb15b5d6b7b5ae41
Amandine Arias <amandine.arias@gmail.com>
2021-10-04 16:48:05 +0200

    docs (remove) : create new branch for it

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-export const getUserById = (userId) => {
-  return new Promise((resolve, reject) => {
-    fetch (`http://localhost:3000/user/${userId}`)
-    .then ((result) => result.json())
-    .then((data) => console.log(data))
-  });
-};
