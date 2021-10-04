19545080df0f877ed68692aae4460429aad446ce
Amandine Arias <amandine.arias@gmail.com>
2021-10-04 16:43:47 +0200

    feat (Routing / Nesting Routing) : handle custom Error & Underconstruction page with relevant redirection

---
+++ src/Pages/EnConstruction.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import '../Styles/alerte.css'
+
+export default function EnConstruction(props) {
+
+  // console.log(props);
+
+  let { id, topicId } = useParams();
+
+setTimeout(() => {
+  props.history.push(`/user/${id}`)
+}, 3000);
+
+  return (
+    <main>
+      <h1 className="alert-msg">Page {topicId} en cours de construction</h1>
+      <p className="alert-msg info-redirection">Redirection vers le tableau de bord dans 2s.</p>
+    </main>
+  );
+}
