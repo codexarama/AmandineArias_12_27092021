b0252b20bbbc0f63ce2a5003008dd230c35e3164
Amandine Arias <amandine.arias@gmail.com>
2021-10-04 16:56:04 +0200

    work in progress (fetch) : get & display backend data

---
+++ src/Services/api.js
@@ -0,0 +1,32 @@
+export const getUserById = (userId) => {
+  // return new Promise((resolve, reject) => {
+    fetch(`http://localhost:3000/user/${userId}`)
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+      // .then(
+      //   (data) => {
+      //     data.ok ? resolve(data) : reject(new Error('error'));
+      //   },
+      //   (error) => {
+      //     reject(new Error(error.message));
+      //   }
+      // ); // undefined
+  // });
+};
+
+// import React, { useEffect, useState } from 'react';
+
+// const GetUserById = (userId) => {
+//     const [data, setData] = useState(null);
+//     useEffect(() => {
+//       const fetchData = async () => {
+//         const response = await fetch(`http://localhost:3000/user/${userId}`);
+//         const newData = await response.json();
+//         setData(newData);
+//       };
+//       fetchData();
+//     }, [userId, userId.id]);
+//     return data ? <div>{data.name}</div> : null;
+// };
+
+// export default GetUserById;
