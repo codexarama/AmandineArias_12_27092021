f284642977db928eef73031da39b5d88ea15a5d2
Amandine Arias <amandine.arias@gmail.com>
2021-10-04 16:38:07 +0200

    refactor (code) : less and best

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function Header({firstName, msg}) {
+export default function Header({name}) {
+
     return (
         <header>
-            <h1>Bonjour <span className='user-name'>{firstName}</span></h1>
-            <h2 className='welcome-message'>{msg}</h2>
+            <h1>Bonjour <span className='user-name'>{name}</span></h1>
+            <h2 className='welcome-message'>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
         </header>
     )
 }
