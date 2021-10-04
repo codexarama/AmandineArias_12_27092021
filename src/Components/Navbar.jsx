95c9fad7204c8bfaad295bb64735097e322ba020
Amandine Arias <amandine.arias@gmail.com>
2021-10-04 16:35:49 +0200

    rename (component & associated styles) : Menu > Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link, NavLink, useRouteMatch } from 'react-router-dom';
+import logo from '../Assets/logo.svg';
+import '../Styles/navbar.css';
+import '../Styles/alerte.css';
+
+export default function Menu() {
+  let { url } = useRouteMatch();
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="logo Sport See" />
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav w-100 d-flex justify-content-evenly">
+            <Link className="nav-link" to="/">
+              Accueil
+            </Link>
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              to={`${url}/profil`}
+            >
+              Profil
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              activeClassName={'active'}
+              to={`${url}/réglages`}
+            >
+              Réglages
+            </NavLink>
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              to={`${url}/communauté`}
+            >
+              Communauté
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
