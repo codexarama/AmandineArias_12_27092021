832c19543c2156cf9ad86eb226be7ca7cc411387
Amandine Arias <amandine.arias@gmail.com>
2021-10-05 15:38:36 +0200

    fix (msg) : replace hard data by variable

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function Header({name}) {
+export default function Header({name, msg}) {
+// export default function Header({name}) {
 
     return (
         <header>
             <h1>Bonjour <span className='user-name'>{name}</span></h1>
-            <h2 className='welcome-message'>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2>
+            <h2 className='welcome-message'>{msg}</h2>
+            {/* <h2 className='welcome-message'>Félicitation ! Vous avez explosé vos objectifs hier 👏</h2> */}
         </header>
     )
 }
+
