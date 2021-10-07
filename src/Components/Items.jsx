ff433d9bb6a7d71abc4238f6cf33a1e8f2d26db2
Amandine Arias <amandine.arias@gmail.com>
2021-10-07 16:29:51 +0200

    fix (sidebar) : manage string and object props | maj style

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -8,15 +8,18 @@ import React from 'react';
  * @param {string} props.copyright > copyrigth text
  * @returns {Reactnode} jsx injected in DOM
  */
-export default function Items({ icon, alt, copyright }) {
+// export default function Items({ index, id, icon, alt }) {
+export default function Items({ index, id, icon, alt, copyright }) {
+  // copyright = 'Copyright, SportSee 2020'
   return (
     <>
       {typeof copyright !== 'string' ? (
-        <div className="sidebar-item">
-          <img className="sidebar-icon" src={icon} alt={alt} />
+        <div key={index} className="sidebar-item aside-item">
+          <img id={id} className="sidebar-icon aside-icon" src={icon} alt={alt} />
         </div>
       ) : (
         <div className="copyright">
+          {/* <span>"Copyright, SportSee 2020"</span> */}
           <span>{copyright}</span>
         </div>
       )}
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import Item from './Items';
+import Items from './Items';
 import Yoga from '../Assets/icon_yoga.svg';
 import Swim from '../Assets/icon_swim.svg';
 import Cycling from '../Assets/icon_cycling.svg';
 import Bobybuilding from '../Assets/icon_bodybuilding.svg';
 import '../Styles/sidebar.css';
 
-export default function Sidebar() {
-  const activity = [Yoga, Swim, Cycling, Bobybuilding];
+const activities = [Yoga, Swim, Cycling, Bobybuilding];
+
+export default function Sidebar({ activityName }) {
+  activityName = ['yoga', 'natation', 'cyclisme', 'musculation'];
+
   return (
-    <aside className="sidebar">
-      {activity.map((sport) => (
-        <Item key={sport} icon={sport} alt={`${sport} icon`} />
+    <nav className="sidebar">
+      {activities.map((icon, index) => (
+        <Items key={index} icon={icon} alt={"icone " + activityName[index]} />
       ))}
-    </aside>
+    </nav>
   );
 }
