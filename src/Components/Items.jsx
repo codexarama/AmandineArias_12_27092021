41efb1dc0bfe7fe8d2e57ceadc9059fb451f069e
Amandine Arias <amandine.arias@gmail.com>
2021-10-07 17:30:57 +0200

    fix (sidebar) : remove conditional display based on type of content

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -8,21 +8,11 @@ import React from 'react';
  * @param {string} props.copyright > copyrigth text
  * @returns {Reactnode} jsx injected in DOM
  */
-// export default function Items({ index, id, icon, alt }) {
-export default function Items({ index, id, icon, alt, copyright }) {
-  // copyright = 'Copyright, SportSee 2020'
+
+ export default function Items({ index, id, icon, alt }) {
   return (
-    <>
-      {typeof copyright !== 'string' ? (
-        <div key={index} className="sidebar-item aside-item">
-          <img id={id} className="sidebar-icon aside-icon" src={icon} alt={alt} />
-        </div>
-      ) : (
-        <div className="copyright">
-          {/* <span>"Copyright, SportSee 2020"</span> */}
-          <span>{copyright}</span>
-        </div>
-      )}
-    </>
+    <div key={index} className="sidebar-item aside-item">
+      <img id={id} className="sidebar-icon aside-icon" src={icon} alt={alt} />
+    </div>
   );
 }
