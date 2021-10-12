15b0ecaa617b45a54e988bad4d741bfad9e8f9a9
Amandine Arias <amandine.arias@gmail.com>
2021-10-12 17:03:24 +0200

    feat (Activity) : manage loading cases

--- src/Components/USER/Activity.jsx
+++ src/Components/USER/Activity.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Erreur404 from '../../Pages/Erreur404';
 import { useParams } from 'react-router-dom';
 import { useFetch } from '../../Services/api';
 import '../../Styles/graphs.css';
@@ -17,7 +18,7 @@ import {
 export default function DailyActivity() {
   let userId = useParams().id;
   const url = 'http://localhost:3000/user/';
-  const { data, isLoading } = useFetch(`${url}${userId}/activity`);
+  const { data, isLoading, hasError } = useFetch(`${url}${userId}/activity`);
 
   if (!isLoading) {
     data.sessions.forEach((date) => {
@@ -25,64 +26,79 @@ export default function DailyActivity() {
       const [yyyy, mm, dd] = date.day.split('-');
       date.name = `${dd}/${mm}`;
     });
-    console.log(data);
+    // console.log(data);
   }
 
   return (
-    <section className="daily-activity" style={{ width: '75%', height: 300, marginRight: '2rem' }}>
-      <h3 className="daily-activity--title" >Activité quotidienne</h3>
-      <ResponsiveContainer>
-        <BarChart data={data.sessions} barGap={8}>
-          <XAxis dataKey="name" stroke="grey" tickLine={false} dy={10} />
-          <YAxis
-            yAxisId="poids"
-            domain={['dataMin -2', 'dataMax + 1']}
-            orientation="right"
-            axisLine={false}
-            tickLine={false}
-            dx={10}
-            dy={-4}
-          />
-          <YAxis
-            yAxisId="calories"
-            domain={['dataMin -25', 'dataMax + 25']}
-            orientation="left"
-            axisLine={false}
-            tickLine={false}
-            dx={-10}
-            dy={-4}
-          />
-          <Tooltip wrapperStyle={{ width: 130 }} content={<CustomTooltip />} />
-          <Legend
-            width={'60%'}
-            iconType={'circle'}
-            iconSize={'.5rem'}
-            wrapperStyle={{
-              top: '-15%',
-              right: "1rem",
-              lineHeight: '40px',
-            }}
-          />
-          <CartesianGrid stroke="#ccc" vertical={false} />
-          <Bar
-            yAxisId="calories"
-            name="Calories brûlées (kCal)"
-            dataKey="calories"
-            fill="black"
-            barSize={8}
-            radius={[50, 50, 0, 0]}
-          />
-          <Bar
-            yAxisId="poids"
-            name="Poids (kg)"
-            dataKey="kilogram"
-            fill="red"
-            barSize={8}
-            radius={[50, 50, 0, 0]}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-    </section>
+    <>
+      {isLoading ? (
+        <span>Chargement en cours...</span>
+      ) : hasError ? (
+        <Erreur404 />
+      ) : data === undefined ? (
+        <span>L'utilisateur que vous recherchez n'est pas enregistré</span>
+      ) : (
+        <>
+          <div className="daily-activity">
+            <h3 className="daily-activity--title">Activité quotidienne</h3>
+            <ResponsiveContainer>
+              <BarChart data={data.sessions} barGap={8}>
+                <XAxis dataKey="name" stroke="grey" tickLine={false} dy={10} />
+                <YAxis
+                  yAxisId="poids"
+                  domain={['dataMin -2', 'dataMax + 1']}
+                  orientation="right"
+                  axisLine={false}
+                  tickLine={false}
+                  dx={10}
+                  dy={-4}
+                />
+                <YAxis
+                  yAxisId="calories"
+                  domain={['dataMin -25', 'dataMax + 25']}
+                  orientation="left"
+                  axisLine={false}
+                  tickLine={false}
+                  dx={-10}
+                  dy={-4}
+                />
+                <Tooltip
+                  wrapperStyle={{ width: 130 }}
+                  content={<CustomTooltip />}
+                />
+                <Legend
+                  width={'60%'}
+                  iconType={'circle'}
+                  iconSize={'.5rem'}
+                  wrapperStyle={{
+                    top: '-15%',
+                    right: '1rem',
+                    lineHeight: '40px',
+                  }}
+                />
+                <CartesianGrid stroke="#ccc" vertical={false} />
+                <Bar
+                  yAxisId="calories"
+                  name="Calories brûlées (kCal)"
+                  dataKey="calories"
+                  fill="black"
+                  barSize={8}
+                  radius={[50, 50, 0, 0]}
+                />
+                <Bar
+                  yAxisId="poids"
+                  name="Poids (kg)"
+                  dataKey="kilogram"
+                  fill="red"
+                  barSize={8}
+                  radius={[50, 50, 0, 0]}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
