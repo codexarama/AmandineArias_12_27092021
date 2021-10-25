23d5fa0839acf86520887f5163c542e154d6d157
Amandine Arias <amandine.arias@gmail.com>
2021-10-25 21:06:42 +0200

    refactor (data focused Routes) : separate render from App Routes by creating specific StatFocus component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Accueil from '../Pages/Accueil';
 
-import logo from '../Assets/logo.svg';
-import Banner from '../Components/UI/Banner';
-import DailyActivity from '../Components/USER/Activity';
-import Average from '../Components/USER/Average';
-import Performance from '../Components/USER/Performance';
-import Score from '../Components/USER/Score';
-import Health from '../Components/USER/Health';
+import { statsFocus } from '../Components/USER/StatsFocus';
 
 import Dashboard from '../Pages/Dashboard';
 
@@ -18,100 +12,6 @@ import Erreur404 from '../Pages/Erreur404';
 
 import './App.css';
 import '../Styles/banner.css';
-
-const statsFocus = [
-  {
-    path: '/user/:id/activity',
-    exact: true,
-    main: () => (
-      <main className="data-focused">
-        <Banner logo={logo} message="Activité quotidienne" />
-        <section
-          style={{
-            marginTop: '12rem',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            width: '75%',
-          }}
-        >
-          <DailyActivity />
-        </section>
-      </main>
-    ),
-  },
-  {
-    path: '/user/:id/average-sessions',
-    exact: true,
-    main: () => (
-      <main className="data-focused">
-        <Banner logo={logo} message="Sessions moyennes" />
-        <section
-          style={{
-            marginTop: '12rem',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <Average className="focus" />
-        </section>
-      </main>
-    ),
-  },
-  {
-    path: '/user/:id/activities',
-    exact: true,
-    main: () => (
-      <main className="data-focused">
-        <Banner logo={logo} message="Performances" />
-        <section
-          style={{
-            marginTop: '12rem',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <Performance />
-        </section>
-      </main>
-    ),
-  },
-  {
-    path: '/user/:id/today-score',
-    exact: true,
-    main: () => (
-      <main className="data-focused">
-        <Banner logo={logo} message="Score du jour" />
-        <section
-          style={{
-            marginTop: '12rem',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <Score />
-        </section>
-      </main>
-    ),
-  },
-  {
-    path: '/user/:id/key-data',
-    exact: true,
-    main: () => (
-      <main className="data-focused">
-        <Banner logo={logo} message="Indicateurs physiologiques" />
-        <section
-          style={{
-            marginTop: '12rem',
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <Health />
-        </section>
-      </main>
-    ),
-  },
-];
 
 export default function App() {
   return (
@@ -126,7 +26,12 @@ export default function App() {
             key={index}
             path={route.path}
             exact={route.exact}
-            children={<route.main />}
+            strict={route.strict}
+            children={
+              <main className="data-focused">
+                <route.focus />
+              </main>
+            }
           />
         ))}
 
---
+++ src/Components/USER/StatsFocus.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+import logo from '../../Assets/logo.svg';
+import Banner from '../UI/Banner';
+import DailyActivity from './Activity';
+import Average from './Average';
+import Performance from './Performance';
+import Score from './Score';
+import Health from './Health';
+
+import '../../Styles/banner.css';
+
+const mainStyle = {
+  marginTop: '12rem',
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+export const statsFocus = [
+  {
+    path: '/user/:id/activity',
+    exact: true,
+    strict: true,
+    focus: () => (
+      <>
+        <Banner logo={logo} message="Activité quotidienne" />
+        <section
+          style={{
+            marginTop: '12rem',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            width: '75%',
+          }}
+        >
+          <DailyActivity />
+        </section>
+      </>
+    ),
+  },
+  {
+    path: '/user/:id/average-sessions',
+    exact: true,
+    strict: true,
+    focus: () => (
+      <>
+        <Banner logo={logo} message="Sessions moyennes" />
+        <section style={mainStyle}>
+          <Average/>
+        </section>
+      </>
+    ),
+  },
+  {
+    path: '/user/:id/activities',
+    exact: true,
+    strict: true,
+    focus: () => (
+      <>
+        <Banner logo={logo} message="Performances" />
+        <section style={mainStyle}>
+          <Performance />
+        </section>
+      </>
+    ),
+  },
+  {
+    path: '/user/:id/today-score',
+    exact: true,
+    strict: true,
+    focus: () => (
+      <>
+        <Banner logo={logo} message="Score du jour" />
+        <section style={mainStyle}>
+          <Score />
+        </section>
+      </>
+    ),
+  },
+  {
+    path: '/user/:id/key-data',
+    exact: true,
+    strict: true,
+    focus: () => (
+      <>
+        <Banner logo={logo} message="Indicateurs physiologiques" />
+        <section style={mainStyle}>
+          <Health />
+        </section>
+      </>
+    ),
+  },
+];
